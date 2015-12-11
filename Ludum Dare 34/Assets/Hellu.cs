25f94eac9a7144d569d58caed965fa6e548cfde9
OscarLundberg <info@oscarlundberg.com>
2015-12-11 21:20:07 +0100

    Recreated Project

---
+++ Ludum Dare 34/Assets/Hellu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hellu : MonoBehaviour {
+	public int LD;
+	// Use this for initialization
+	void Start () {
+		LD = 34;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
